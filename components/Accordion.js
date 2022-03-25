581b3f5005ae04f82662d625a4f4e0e31ddff9ee
Aidan Chang <84875731+aidanschang@users.noreply.github.com>
2022-03-24 19:23:38 -0700

    Add files via upload

---
+++ components/Accordion.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+
+const Accordion = ({items}) =>{
+    //Array destructure: no array is declared inside below squre bracket
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    //returns all items inside items through map
+    const renderedItems = items.map((item, index) => {
+        const active = index ===activeIndex? 'active': '';
+
+        
+        return <React.Fragment key={item.title}> {/*React.Fragment was used in replace of div was to avoid a css colision from semantic-ui*/}
+            <div 
+                className={`title ${active}`}
+                onClick= {() => onTitleClick(index)}
+            >
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+
+            <div className={`content ${active}`}>
+                <p> {item.content}</p>
+            </div>
+        </React.Fragment>
+    })
+
+    return <div className="ui styled accordion">
+        {renderedItems}
+    </div>;
+};
+
+export default Accordion;
